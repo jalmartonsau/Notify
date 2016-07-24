195aca410d9ca8dd984b803a9d67edd05c3c696f
Jalmar Tõnsau <jalmartonsau@outlook.com>
2016-07-24 19:05:47 +0300

    ModelBase added. OnPropertyChanged

---
+++ Notify/Model/InsertVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notify.Model
+{
+    class InsertVM : ModelBase
+    {
+
+        private string _Insert;
+        public string Insert { get { return _Insert; } set { _Insert = value; OnPropertyChanged("Insert"); } }
+
+
+    }
+}
---
+++ Notify/Model/ModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notify.Model
+{
+    class ModelBase
+    {
+
+        #region OnPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+    }
+}
